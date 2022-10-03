c16d70f3e5e8fcd6ddf06e66dc5af289aacc80da
jordithc <tsehoicheung98@gmail.com>
2022-10-02 22:41:39 -0400

    Final

--- MyProtfilo/routes/index.js
+++ MyProtfilo/routes/index.js
@@ -1,9 +1,38 @@
+/* index.js Tse Hoi Cheung 301234975 2OCT2022--> */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: "Home" });
+});
+
+
+/* GET About Me page. */
+router.get("/about", function(req, res, next){
+  res.render("index", { title: 'About'});
+});
+
+/* GET Project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Service page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get("/contact", function(req, res, next){
+  res.render("index", {title: 'Contact'});
+});
+
 
 module.exports = router;
