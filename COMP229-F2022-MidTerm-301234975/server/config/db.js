7d49db2833fc590c1ab7304305646c07b67b47c6
jordithc <tsehoicheung98@gmail.com>
2022-10-28 23:43:31 -0400

    Final

--- COMP229-F2022-MidTerm-301234975/server/config/db.js
+++ COMP229-F2022-MidTerm-301234975/server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  //URI: "mongodb://127.0.0.1/product_store",
- URI: "mongodb+srv://tsehc_01:770MotIeh1nRvwzx@cluster0.yrdihmw.mongodb.net/product_store?retryWrites=true&w=majority"
+  URI: "mongodb://127.0.0.1/product_store",
+ //URI: "mongodb+srv://tsehc_01:770MotIeh1nRvwzx@cluster0.yrdihmw.mongodb.net/product_store?retryWrites=true&w=majority"
 };
